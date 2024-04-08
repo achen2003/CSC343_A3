82cd6840cbc518bfbf9d6b9ea296d2a87f8e3b2c
Khushi Patel <pate1667@teach.cs.toronto.edu>
2024-04-07 23:46:31 -0400

    removed spaces

--- data.sql
+++ data.sql
@@ -33,4 +33,36 @@ SET SEARCH_PATH TO A3Conference;
 \Copy ConferenceChairs FROM 'data/ConferenceChairs.csv' WITH CSV DELIMITER ',' HEADER;
 
 
+=======
+-- Import Data for Holding
+\copy Author FROM 'data/Author.csv' WITH CSV DELIMITER ',' HEADER;
 
+\copy Contributor FROM 'data/Contributor.csv' WITH CSV DELIMITER ',' HEADER;
+
+\copy Submission FROM 'data/Submission.csv' WITH CSV DELIMITER ',' HEADER;
+
+\copy Conference FROM 'data/Conference.csv' WITH CSV DELIMITER ',' HEADER;
+
+\copy Reviewer FROM 'data/Reviewer.csv' WITH CSV DELIMITER ',' HEADER;
+
+\copy Review FROM 'data/Review.csv' WITH CSV DELIMITER ',' HEADER;
+
+\copy Session FROM 'data/Session.csv' WITH CSV DELIMITER ',' HEADER;
+
+\copy Presentation FROM 'data/Presentation.csv' WITH CSV DELIMITER ',' HEADER;
+
+\copy Registration FROM 'data/Registration.csv' WITH CSV DELIMITER ',' HEADER;
+
+\copy Attendee FROM 'data/Attendee.csv' WITH CSV DELIMITER ',' HEADER;
+
+\copy Workshop FROM 'data/Workshop.csv' WITH CSV DELIMITER ',' HEADER;
+
+\copy Facilitator FROM 'data/Facilitator.csv' WITH CSV DELIMITER ',' HEADER;
+
+\copy Committee FROM 'data/Committee.csv' WITH CSV DELIMITER ',' HEADER;
+
+\copy ConferenceChairs FROM 'data/ConferenceChairs.csv' WITH CSV DELIMITER ',' HEADER;
+
+\copy WorkshopRegistration FROM 'data/WorkshopRegistration.csv' WITH CSV DELIMITER ',' HEADER;
+
+\copy CommitteeMember FROM 'data/CommitteeMember.csv' WITH CSV DELIMITER ',' HEADER;
