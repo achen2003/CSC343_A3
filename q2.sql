10a7fddfcea5fd88fb7941900efffb49b41ae64b
Alex Chen <achen.phi@gmail.com>
2024-04-08 13:16:19 -0400

    Changed some attribute names

--- q2.sql
+++ q2.sql
@@ -2,8 +2,8 @@ SET SEARCH_PATH TO A3Conference, public;
 
 -- Query for reporting the number of conferences each person has attended
 SELECT
-    a.att_id,
-    a.att_name,
+    a.att_id AS attribute_id,
+    a.att_name AS attribute_name, 
     COUNT(DISTINCT r.conf_id) AS conference_count
 FROM Attendee a
 LEFT JOIN Registration r ON a.att_id = r.att_id
