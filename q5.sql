292ec44c8ca568d01bc509b5c7969ced66952c7a
Alex Chen <achen.phi@gmail.com>
2024-04-08 13:54:01 -0400

    Fixed q5

--- q5.sql
+++ q5.sql
@@ -2,20 +2,30 @@ SET SEARCH_PATH TO A3Conference, public;
 
 -- Average number of papers in a paper session for each conference
 CREATE VIEW AvgPapersPerPaperSession AS
-    SELECT s.conf_id, AVG(CASE WHEN s.sess_type = 'paper session' THEN 1 ELSE 0 END) AS avg_papers_per_session
-    FROM Session s
-    JOIN Presentation p ON s.sess_id = p.sess_id
-    JOIN Submission sub ON p.sub_id = sub.sub_id
-    WHERE s.sess_type = 'paper session'
+    SELECT s.conf_id, 
+           AVG(papers_count) AS avg_papers_per_session
+    FROM (
+        SELECT s.sess_id, COUNT(p.pres_id) AS papers_count
+        FROM Session s
+        JOIN Presentation p ON s.sess_id = p.sess_id
+        WHERE s.sess_type = 'paper session'
+        GROUP BY s.sess_id
+    ) AS sub_query
+    JOIN Session s ON sub_query.sess_id = s.sess_id
     GROUP BY s.conf_id;
 
 -- Average number of posters in a poster session for each conference
 CREATE VIEW AvgPostersPerPosterSession AS
-    SELECT s.conf_id, AVG(CASE WHEN s.sess_type = 'poster session' THEN 1 ELSE 0 END) AS avg_posters_per_session
-    FROM Session s
-    JOIN Presentation p ON s.sess_id = p.sess_id
-    JOIN Submission sub ON p.sub_id = sub.sub_id
-    WHERE s.sess_type = 'poster session'
+    SELECT s.conf_id, 
+           AVG(posters_count) AS avg_posters_per_session
+    FROM (
+        SELECT s.sess_id, COUNT(p.pres_id) AS posters_count
+        FROM Session s
+        JOIN Presentation p ON s.sess_id = p.sess_id
+        WHERE s.sess_type = 'poster session'
+        GROUP BY s.sess_id
+    ) AS sub_query
+    JOIN Session s ON sub_query.sess_id = s.sess_id
     GROUP BY s.conf_id;
 
 -- Query to report the average number of papers in a paper session and the average number of posters in a poster session for each occurrence of a conference
