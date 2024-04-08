c9e4c4b08d5b5b34d9de1691c219520f7384130e
Khushi Patel <pate1667@teach.cs.toronto.edu>
2024-04-08 09:19:30 -0400

    updated data, tested Q1,2,5

--- q1.sql
+++ q1.sql
@@ -20,7 +20,7 @@ SET SEARCH_PATH TO A3Conference, public;
 
 SELECT 
     c.cname AS conference_name,
-    EXTRACT(YEAR FROM s.sub_date) AS submission_year,
+    EXTRACT(YEAR FROM c.cstart_date) AS submission_year,
     CASE 
         WHEN COUNT(*) = 0 THEN 0 
         ELSE ROUND(
@@ -28,18 +28,6 @@ SELECT
             2
         ) 
     END AS acceptance_percentage
-=======
-SELECT
-    c.conf_id,
-    EXTRACT(YEAR FROM c.cstart_date) AS conference_year,
-    COUNT(DISTINCT s.sub_id) AS total_submissions,
-    COUNT(DISTINCT CASE WHEN r.decision = 'accept' THEN s.sub_id END) AS accepted_submissions,
-    ROUND(
-        100.0 * COUNT(DISTINCT CASE WHEN r.decision = 'accept' THEN s.sub_id END) /
-        COUNT(DISTINCT s.sub_id),
-        2
-    ) AS acceptance_percentage
->>>>>>> Stashed changes
 FROM Conference c
 LEFT JOIN Submission s ON c.conf_id = s.conf_id
 LEFT JOIN Review r ON s.sub_id = r.sub_id
